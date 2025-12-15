97867473fc12644c003720d54cb40ec4072f3a3b
memento_mori <posotpia7139@gmail.com>
2025-12-15 20:25:09 +0900

    toc 3->2 camus profile

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,11 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Jamesolo",
-	subtitle: "Demo Site",
+	subtitle: "Solo Creation Log.",
 	lang: "ko", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: false,
@@ -27,7 +27,7 @@ export const siteConfig: SiteConfig = {
 	},
 	toc: {
 		enable: true, // Display the table of contents on the right side of the post
-		depth: 3, // Maximum heading depth to show in the table, from 1 to 3
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/camus.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "James Lee",
 	bio: "solo indie developer.",
 	links: [
