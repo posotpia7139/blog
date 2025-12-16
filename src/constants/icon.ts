2fd3c40a972cd53028b5e1df8fce80068eda532b
memento_mori <posotpia7139@gmail.com>
2025-12-16 17:24:47 +0900

    light icon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -1,26 +1,26 @@
 import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
-	// {
-	// 	src: "/favicon/favicon-light-32.png",
-	// 	theme: "light",
-	// 	sizes: "32x32",
-	// },
-	// {
-	// 	src: "/favicon/favicon-light-128.png",
-	// 	theme: "light",
-	// 	sizes: "128x128",
-	// },
-	// {
-	// 	src: "/favicon/favicon-light-180.png",
-	// 	theme: "light",
-	// 	sizes: "180x180",
-	// },
-	// {
-	// 	src: "/favicon/favicon-light-192.png",
-	// 	theme: "light",
-	// 	sizes: "192x192",
-	// },
+	{
+		src: "/favicon/favicon-light-32.png",
+		theme: "light",
+		sizes: "32x32",
+	},
+	{
+		src: "/favicon/favicon-light-128.png",
+		theme: "light",
+		sizes: "128x128",
+	},
+	{
+		src: "/favicon/favicon-light-180.png",
+		theme: "light",
+		sizes: "180x180",
+	},
+	{
+		src: "/favicon/favicon-light-192.png",
+		theme: "light",
+		sizes: "192x192",
+	},
 	{
 		src: "/favicon/favicon-dark-32.png",
 		theme: "dark",
