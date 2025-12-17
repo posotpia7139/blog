addb45cc430b65e1825fb25c819c977a299fbbb4
memento_mori <posotpia7139@gmail.com>
2025-12-17 16:20:51 +0900

    @apply 지시어 사용 시, 해당 클래스가 Tailwind 설정(tailwind.config.cjs)에 정의되어 있지 않은 경우, 컴파일 오류가      발생함.    * 사용자 정의 컴포넌트 클래스는 tailwind.config.cjs의 plugins 섹션에서 addComponents를 사용하여 정의

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,5 +10,15 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    function ({ addComponents }) {
+      addComponents({
+        ".link": {
+          "@apply font-medium text-[var(--primary)] underline decoration-[var(--link-underline)] decoration-1 decoration-dashed underline-offset-4":
+            {},
+        },
+      })
+    },
+  ],
 }
