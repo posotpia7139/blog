715fdc686c55292008dcb90f602af303cb0822f7
memento_mori <posotpia7139@gmail.com>
2025-12-19 01:55:36 +0900

    블로그 홈 이름 변경 대문자 (Present Flame)

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "present flame",
+	title: "Present Flame",
 	subtitle: "A Solo Creation Log",
 	lang: "ko", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
