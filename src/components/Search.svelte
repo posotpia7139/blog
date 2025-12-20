d21f9df7c592a6676aacd77859427705ee5cc37b
memento_mori <posotpia7139@gmail.com>
2025-12-20 15:15:55 +0900

    검색 입력란의 대비를 개선하여 가독성을 높였습니다. 검색 아이콘과 입력 텍스트 색상을 대비가 높은 색조로 업데이트하여 밝은 테마와 어두운 테마 모두에서 접근성과 시각적 명확성을 향상시켰습니다. 자리 표시자 텍스트 색상을 조정하여 입력 텍스트와 더 잘 구분되도록 했습니다.

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -156,10 +156,10 @@ $: if (initialized && keywordMobile) {
       bg-black/[0.04] hover:bg-black/[0.06] focus-within:bg-black/[0.06]
       dark:bg-white/5 dark:hover:bg-white/10 dark:focus-within:bg-white/10
 ">
-    <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/50 dark:text-white/50"></Icon>
+    <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/90 dark:text-white/90"></Icon>
     <input placeholder="{i18n(I18nKey.search)}" bind:value={keywordDesktop} on:focus={() => search(keywordDesktop, true)}
-           class="transition-all pl-10 text-sm bg-transparent outline-0
-         h-full w-40 active:w-60 focus:w-60 text-black/50 dark:text-white/50"
+           class="transition-all pl-10 text-sm bg-transparent outline-0 font-medium
+         h-full w-40 active:w-60 focus:w-60 text-black/90 dark:text-white/90 placeholder:text-black/60 dark:placeholder:text-white/60"
     >
 </div>
 
@@ -178,10 +178,10 @@ top-20 left-4 md:left-[unset] right-4 shadow-2xl rounded-2xl p-2">
       bg-black/[0.04] hover:bg-black/[0.06] focus-within:bg-black/[0.06]
       dark:bg-white/5 dark:hover:bg-white/10 dark:focus-within:bg-white/10
   ">
-        <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/50 dark:text-white/50"></Icon>
+        <Icon icon="material-symbols:search" class="absolute text-[1.25rem] pointer-events-none ml-3 transition my-auto text-black/90 dark:text-white/90"></Icon>
         <input placeholder="Search" bind:value={keywordMobile}
-               class="pl-10 absolute inset-0 text-sm bg-transparent outline-0
-               focus:w-60 text-black/50 dark:text-white/50"
+               class="pl-10 absolute inset-0 text-sm bg-transparent outline-0 font-medium
+               focus:w-60 text-black/90 dark:text-white/90 placeholder:text-black/60 dark:placeholder:text-white/60"
         >
     </div>
 
