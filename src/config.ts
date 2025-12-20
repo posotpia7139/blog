31697ff1eb0e8568b39b19fe2c95cb645f4803a3
memento_mori <posotpia7139@gmail.com>
2025-12-20 14:42:49 +0900

    Updates GitHub profile link in social config
    Replaces the previous GitHub URL with a new one to reflect the correct profile.
    Ensures that the displayed link directs users to the intended repository or account.

--- src/config.ts
+++ src/config.ts
@@ -71,7 +71,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/posotpia7139",
 		},
 	],
 };
